66d60c83c1478e7e9bfb3375b0e6f351f62d57c1
fadhrusd7 <fadhilrusadi5@gmail.com>
2024-12-04 13:22:41 +0700

    Update algortima.py

--- algortima.py
+++ algortima.py
@@ -8,7 +8,19 @@ merah = high itensity, low intensity
 
 cari kontur warna terpilih
 cari area kontur terbesar
-cari moment kontur terbesar
+cari moment kontur terbesar (titik pusat)
 
 bagi frame jadi 4 bagian
 
+awal dari jalannya algoritma = bola hijau berada di kiri frame dan titik pusatnya di kordinat (sekian) dengan ukuran bola hijau (sekian) 
+                              &&
+                              bola merah berda di kanan frame dan titik pusatnya di kordinat (sekian) dengan ukuran bola merah (sekian)
+                              = maju
+if
+ada bola ditengah frame
+  if bola merah 
+sesuaikan titik pusat bola merah dan maju sampai ukuran boal merah (sekian)
+belok ke kiri sebesar 60 derajat
+dan cari bola hijau, sesuaikan titik pusat bola hijau dan maju sampai ukuran bola hijau (sekian)
+dan belok ke kanan sebesar 60 derajat
+ if 
